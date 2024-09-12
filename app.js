5c54432489df62cdb09566d03ea60861dcacda53
Unitech Complex <unitech.complex@gmail.com>
2024-09-12 20:36:03 +0600

    Update app.js

--- app.js
+++ app.js
@@ -54,17 +54,17 @@ function showSlider(type){
 
 let home = document.querySelector('#home') ;
 home.addEventListener('click',()=>{
- window.location.href = '/index.html';
+ window.location.href = '/SA/index.html';
   
 })
 let about = document.querySelector('.about') ;
 about.addEventListener('click',()=>{
-  window.location.href = '/about.html';
+  window.location.href = '/SA/about.html';
 })
 
 let contact = document.querySelector('.contact') ;
 contact.addEventListener('click',()=>{
-   window.location.href = '/contact.html';
+   window.location.href = '/SA/contact.html';
 })
 
 
